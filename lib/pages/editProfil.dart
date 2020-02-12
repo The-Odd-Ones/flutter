3894dc7897f95938d9927bfd04df4ce607a04223
hanialazreg <lazreghania@gmail.com>
2020-02-12 09:25:09 +0100

    refactor edit profil with form

--- lib/pages/editProfil.dart
+++ lib/pages/editProfil.dart
@@ -6,6 +6,7 @@ import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 
 class EditProfil extends StatefulWidget {
+
   static const routeName = '/EditProfil';
   @override
   _EditProfilState createState() => _EditProfilState();
@@ -57,120 +58,126 @@ class _ImageCaptureState extends State<ImageCapture> {
 
       ),
           body: Center(
-            child: Column(
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.cyan,
-                        child: Text('Edit',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),)
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.photo_camera),
-                      onPressed: () => _pickImage(ImageSource.gallery),
-                    ),
-                    Container(
+            child: Form(
 
-                      decoration: BoxDecoration(
-                        border: Border(
-                          top: BorderSide(width: 1, color: Colors.white70),
-                          left: BorderSide(width: 1, color: Colors.white70),
-                          right: BorderSide(width: 1, color: Colors.white70),
-                          bottom: BorderSide(width: 1, color: Colors.white70),
-
-                        )
+              child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.cyan,
+                          child: Text('Edit',
+                          style: TextStyle(
+                            color: Colors.white
+                          ),)
                       ),
-                      child: Column(
-                        children: <Widget>[
-                          Text('User Name',
-                          textAlign: TextAlign.left,),
-                      TextField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Enter new username'
+                      IconButton(
+                        icon: Icon(Icons.photo_camera),
+                        onPressed: () => _pickImage(ImageSource.gallery),
+                      ),
+                      Container(
+
+                        decoration: BoxDecoration(
+                          border: Border(
+                            top: BorderSide(width: 1, color: Colors.white70),
+                            left: BorderSide(width: 1, color: Colors.white70),
+                            right: BorderSide(width: 1, color: Colors.white70),
+                            bottom: BorderSide(width: 1, color: Colors.white70),
+
+                          )
                         ),
+                        child: Column(
+                          children: <Widget>[
+                            Text('User Name',
+                            textAlign: TextAlign.left,),
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Enter new username'
+                          ),
+                        )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                              top: BorderSide(width: 1, color: Colors.white70),
+                              left: BorderSide(width: 1, color: Colors.white70),
+                              right: BorderSide(width: 1, color: Colors.white70),
+                              bottom: BorderSide(width: 1, color: Colors.white70),
+
+                            )
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Text(' Name',
+                              textAlign: TextAlign.left,),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Enter new name'
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                              top: BorderSide(width: 1, color: Colors.white70),
+                              left: BorderSide(width: 1, color: Colors.white70),
+                              right: BorderSide(width: 1, color: Colors.white70),
+                              bottom: BorderSide(width: 1, color: Colors.white70),
+
+                            )
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Text('Last Name',
+                              textAlign: TextAlign.left,),
+                            TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Enter new lastname'
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                              top: BorderSide(width: 1, color: Colors.white70),
+                              left: BorderSide(width: 1, color: Colors.white70),
+                              right: BorderSide(width: 1, color: Colors.white70),
+                              bottom: BorderSide(width: 1, color: Colors.white70),
+
+                            )
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Text('Password',
+                              textAlign: TextAlign.left,),
+                            TextFormField(
+                              decoration: InputDecoration(
+
+                                  border: InputBorder.none,
+                                  hintText: 'Enter Password'
+                              ),
+                              obscureText: true,
+                            )
+                          ],
+                        ),
+                      ),
+                      FlatButton(
+
+                        textColor: Colors.white,
+                        color: Colors.green,
+
+                        child: Text('Save changes'),
                       )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border(
-                            top: BorderSide(width: 1, color: Colors.white70),
-                            left: BorderSide(width: 1, color: Colors.white70),
-                            right: BorderSide(width: 1, color: Colors.white70),
-                            bottom: BorderSide(width: 1, color: Colors.white70),
-
-                          )
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Text(' Name',
-                            textAlign: TextAlign.left,),
-                          TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Enter new name'
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border(
-                            top: BorderSide(width: 1, color: Colors.white70),
-                            left: BorderSide(width: 1, color: Colors.white70),
-                            right: BorderSide(width: 1, color: Colors.white70),
-                            bottom: BorderSide(width: 1, color: Colors.white70),
-
-                          )
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Text('Last Name',
-                            textAlign: TextAlign.left,),
-                          TextField(
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Enter new lastname'
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border(
-                            top: BorderSide(width: 1, color: Colors.white70),
-                            left: BorderSide(width: 1, color: Colors.white70),
-                            right: BorderSide(width: 1, color: Colors.white70),
-                            bottom: BorderSide(width: 1, color: Colors.white70),
-
-                          )
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Text('Password',
-                            textAlign: TextAlign.left,),
-                          TextField(
-                            decoration: InputDecoration(
-
-                                border: InputBorder.none,
-                                hintText: 'Enter Password'
-                            ),
-                            obscureText: true,
-                          )
-                        ],
-                      ),
-                    ),
-                    FlatButton(
-
-                      textColor: Colors.white,
-                      child: Text('Save changes'),
-                    )
-                  ],
+                    ],
+              ),
             ),
           ),
 
@@ -180,5 +187,50 @@ class _ImageCaptureState extends State<ImageCapture> {
 }
 
 /*
-
+child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'User Name'),
+                  textInputAction: TextInputAction.next,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some User Name';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) {
+                    _authData['username'] = val;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  textInputAction: TextInputAction.done,
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 6) {
+                      return 'Please enter some password';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) {
+                    _authData['password'] = val;
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Save'),
+                  onPressed: () {
+                    saveForm();
+                  },
+                ),
+                RaisedButton(
+                    child: Text('Create Account'),
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pushReplacementNamed(SignUp.routeName);
+                    })
+              ],
+            ),
+          ),
  */
