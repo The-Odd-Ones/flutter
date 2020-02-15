f905d41a60ac17f3401a9f6b0b03263b7250a64c
insafmechergui <insaf_mechergui@outlook.com>
2020-02-15 14:23:34 +0100

    map

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -89,15 +89,15 @@ class _HomeState extends State<Home> {
                 Navigator.of(context).pushNamed(Events.routeName);
               },
             ),
-            IconButton(
-              icon: Icon(
-                Icons.map,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(MapPage.routeName);
-              },
-            ),
+            // IconButton(
+            //   icon: Icon(
+            //     Icons.map,
+            //     color: Colors.white,
+            //   ),
+            //   onPressed: () {
+            //     Navigator.of(context).pushNamed(MapPage.routeName);
+            //   },
+            // ),
           ],
         ),
         drawer: AppDrawer(),
